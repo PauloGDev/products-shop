9a7f6e84aaea2711a3c37dfa5c2608af5787c7fd
Paulo Guilherme <psnpaulogui@outlook.com>
2024-04-17 20:28:40 -0300

    Post new Categories

--- backend/src/main/java/com/volt/bootcampcrud01/controllers/CategoryController.java
+++ backend/src/main/java/com/volt/bootcampcrud01/controllers/CategoryController.java
@@ -1,18 +1,14 @@
 package com.volt.bootcampcrud01.controllers;
 
 import com.volt.bootcampcrud01.dtos.CategoryDTO;
-import com.volt.bootcampcrud01.entities.Category;
 import com.volt.bootcampcrud01.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,5 +28,12 @@ public class CategoryController {
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
         CategoryDTO entity = categoryService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(entity);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+        dto = categoryService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
